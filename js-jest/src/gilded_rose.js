44401b112b5b474b7d79eba723034117dabed258
Riley Lovell <riley.lovell93@gmail.com>
2023-02-14 11:38:15 -0500

    JS-Jest: HandleNormalItems method

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,17 +10,34 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+  
+  handleNormalItems(item) {
+    let totalDecay = 1; 
+    if (item.sellIn < 0) {
+      totalDecay += 1;
+  }
+    if (item.quality - totalDecay < 0){
+      item.quality = 0;
+    } else {
+        item.quality -= totalDecay;
+      };
+  };
+  
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+        continue;
+      }
+      this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].quality < 0){
         this.items[i].quality = 0;
       };
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.handleNormalItems(this.items[i]);
             if (this.items[i].name.includes('Conjured') && this.items[i].quality > 0){
-              this.items[i].quality = this.items[i].quality - 1;
+              this.items[i].quality = this.items[i].quality - 2;
             }
           }
         }
@@ -41,17 +58,13 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
                 if (this.items[i].name.includes('Conjured') && this.items[i].quality > 0){
-                  this.items[i].quality = this.items[i].quality - 1;
+                  this.items[i].quality = this.items[i].quality - 2;
                 }
               }
             }
